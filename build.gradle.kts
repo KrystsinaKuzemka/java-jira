5038345367cbf0f6910f4b7869130699fc78fa2b
KrystsinaKuzemka <155772649+KrystsinaKuzemka@users.noreply.github.com>
2024-10-31 18:32:50 +0100

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.3.5"
+	id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "com.jira.automation"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-mail")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	implementation("org.springframework.boot:spring-boot-starter-mail")
+	implementation("org.springframework.boot:spring-boot-starter-mail")  // Dodaje wsparcie Spring Boot dla poczty
+	implementation("com.sun.mail:jakarta.mail:2.0.1")  // Dodaje JavaMail API (nowa wersja Jakarta Mail)
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
